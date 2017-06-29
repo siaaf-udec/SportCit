5d159b3c3eb6ed8e9bb810b14252506591bd4e45
FredyHenao <jhongallego45y@hotmail.com>
2017-06-29 15:44:47 -0500

    Configuración del proyecto
    Cambiando estilos e imagenes.

---
+++ public/assets/pages/scripts/login-5.js
@@ -0,0 +1,113 @@
+var Login = function() {
+
+    var handleLogin = function() {
+
+        $('#form-login').validate({
+            errorElement: 'span', //default input error message container
+            errorClass: 'help-block help-block-error', // default input error message class
+            focusInvalid: false, // do not focus the last invalid input
+            rules: {
+                email: {
+                    required: true
+                },
+                password: {
+                    required: true
+                },
+                remember: {
+                    required: false
+                }
+            },
+
+            messages: {
+                email: {
+                    required: "El correo electrónico es requerido."
+                },
+                password: {
+                    required: "La contraseña es requerida."
+                }
+            },
+
+            invalidHandler: function(event, validator) { //display error alert on form submit   
+                $('.alert-danger', $('#form-login')).show();
+            },
+
+            highlight: function(element) { // hightlight error inputs
+                $(element)
+                    .closest('.form-group').addClass('has-error'); // set error class to the control group
+            },
+
+            success: function(label) {
+                label.closest('.form-group').removeClass('has-error');
+                label.remove();
+            },
+
+            errorPlacement: function(error, element) {
+                error.insertAfter(element);
+            },
+
+            submitHandler: function(form) {
+                form.submit(); // form validation success, call ajax form submit
+            }
+        });
+
+        $('#form-login input').keypress(function(e) {
+            if (e.which == 13) {
+                if ($('#form-login').validate().form()) {
+                    $('#form-login').submit(); //form validation success, call ajax form submit
+                }
+                return false;
+            }
+        });
+
+        $('.forget-form input').keypress(function(e) {
+            if (e.which == 13) {
+                if ($('.forget-form').validate().form()) {
+                    $('.forget-form').submit();
+                }
+                return false;
+            }
+        });
+
+        $('#forget-password').click(function(){
+            $('#form-login').hide();
+            $('.forget-form').show();
+        });
+
+        $('#back-btn').click(function(){
+            $('#form-login').show();
+            $('.forget-form').hide();
+        });
+    }
+
+ 
+  
+
+    return {
+        //main function to initiate the module
+        init: function() {
+
+            handleLogin();
+
+            // init background slide images
+            $('.login-bg').backstretch([
+                "assets/pages/img/login/bg1.jpg",
+                "assets/pages/img/login/bg2.jpg",
+                "assets/pages/img/login/bg3.jpg",
+                "assets/pages/img/login/bg4.jpg"
+                ], {
+                  fade: 1000,
+                  duration: 8000
+                }
+            );
+
+            $('.forget-form').hide();
+
+        }
+
+    };
+
+}();
+
+jQuery(document).ready(function() {
+    Login.init();
+});
---
+++ resources/lang/es/validation.php
@@ -0,0 +1,155 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | such as the size rules. Feel free to tweak each of these messages.
+    |
+    */
+
+    'accepted'             => 'El campo :attribute debe ser aceptado.',
+    'active_url'           => 'El campo :attribute no es una URL válida.',
+    'after'                => 'El campo :attribute debe ser una fecha posterior a :date.',
+    'after_or_equal'       => 'El campo :attribute debe ser una fecha posterior o igual a :date.',
+    'alpha'                => 'El campo :attribute sólo debe contener letras.',
+    'alpha_dash'           => 'El campo :attribute sólo debe contener letras, números y guiones.',
+    'alpha_num'            => 'El campo :attribute sólo debe contener letras y números.',
+    'array'                => 'El campo :attribute debe ser un conjunto.',
+    'before'               => 'El campo :attribute debe ser una fecha anterior a :date.',
+    'before_or_equal'      => 'El campo :attribute debe ser una fecha anterior o igual a :date.',
+    'between'              => [
+        'numeric' => 'El campo :attribute tiene que estar entre :min - :max.',
+        'file'    => 'El campo :attribute debe pesar entre :min - :max kilobytes.',
+        'string'  => 'El campo :attribute tiene que tener entre :min - :max caracteres.',
+        'array'   => 'El campo :attribute tiene que tener entre :min - :max ítems.',
+    ],
+    'boolean'              => 'El campo :attribute debe tener un valor verdadero o falso.',
+    'confirmed'            => 'La confirmación de :attribute no coincide.',
+    'date'                 => 'El campo :attribute no es una fecha válida.',
+    'date_format'          => 'El campo :attribute no corresponde al formato :format.',
+    'different'            => 'El campo :attribute y :other deben ser diferentes.',
+    'digits'               => 'El campo :attribute debe tener :digits dígitos.',
+    'digits_between'       => 'El campo :attribute debe tener entre :min y :max dígitos.',
+    'dimensions'           => 'Las dimensiones de la imagen :attribute no son válidas.',
+    'distinct'             => 'El campo :attribute contiene un valor duplicado.',
+    'email'                => 'El campo :attribute no es un correo válido',
+    'exists'               => 'El campo :attribute es inválido.',
+    'file'                 => 'El campo :attribute debe ser un archivo.',
+    'filled'               => 'El campo :attribute es obligatorio.',
+    'image'                => 'El campo :attribute debe ser una imagen.',
+    'in'                   => 'El campo :attribute es inválido.',
+    'in_array'             => 'El campo :attribute no existe en :other.',
+    'integer'              => 'El campo :attribute debe ser un número entero.',
+    'ip'                   => 'El campo :attribute debe ser una dirección IP válida.',
+    'json'                 => 'El campo :attribute debe tener una cadena JSON válida.',
+    'max'                  => [
+        'numeric' => 'El campo :attribute no debe ser mayor a :max.',
+        'file'    => 'El campo :attribute no debe ser mayor que :max kilobytes.',
+        'string'  => 'El campo :attribute no debe ser mayor que :max caracteres.',
+        'array'   => 'El campo :attribute no debe tener más de :max elementos.',
+    ],
+    'mimes'                => 'El campo :attribute debe ser un archivo con formato: :values.',
+    'mimetypes'            => 'El campo :attribute debe ser un archivo con formato: :values.',
+    'min'                  => [
+        'numeric' => 'El tamaño de :attribute debe ser de al menos :min.',
+        'file'    => 'El tamaño de :attribute debe ser de al menos :min kilobytes.',
+        'string'  => 'El campo :attribute debe contener al menos :min caracteres.',
+        'array'   => 'El campo :attribute debe tener al menos :min elementos.',
+    ],
+    'not_in'               => 'El campo :attribute es inválido.',
+    'numeric'              => 'El campo :attribute debe ser numérico.',
+    'present'              => 'El campo :attribute debe estar presente.',
+    'regex'                => 'El formato de :attribute es inválido.',
+    'required'             => 'El campo :attribute es obligatorio.',
+    'required_if'          => 'El campo :attribute es obligatorio cuando :other es :value.',
+    'required_unless'      => 'El campo :attribute es obligatorio a menos que :other esté en :values.',
+    'required_with'        => 'El campo :attribute es obligatorio cuando :values está presente.',
+    'required_with_all'    => 'El campo :attribute es obligatorio cuando :values está presente.',
+    'required_without'     => 'El campo :attribute es obligatorio cuando :values no está presente.',
+    'required_without_all' => 'El campo :attribute es obligatorio cuando ninguno de :values estén presentes.',
+    'same'                 => 'El campo :attribute y :other deben coincidir.',
+    'size'                 => [
+        'numeric' => 'El tamaño de :attribute debe ser :size.',
+        'file'    => 'El tamaño de :attribute debe ser :size kilobytes.',
+        'string'  => 'El campo :attribute debe contener :size caracteres.',
+        'array'   => 'El campo :attribute debe contener :size elementos.',
+    ],
+    'string'               => 'El campo :attribute debe ser una cadena de caracteres.',
+    'timezone'             => 'El :attribute debe ser una zona válida.',
+    'unique'               => 'El campo :attribute ya ha sido registrado.',
+    'uploaded'             => 'Subir :attribute ha fallado.',
+    'url'                  => 'El formato :attribute es inválido.',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom'               => [
+        'attribute-name' => [
+            'rule-name' => 'custom-message',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes'           => [
+        'name'                  => 'nombre',
+        'username'              => 'usuario',
+        'email'                 => 'correo electrónico',
+        'first_name'            => 'nombre',
+        'last_name'             => 'apellido',
+        'password'              => 'contraseña',
+        'password_confirmation' => 'confirmación de la contraseña',
+        'city'                  => 'ciudad',
+        'country'               => 'país',
+        'address'               => 'dirección',
+        'phone'                 => 'teléfono',
+        'mobile'                => 'celular',
+        'age'                   => 'edad',
+        'sex'                   => 'sexo',
+        'gender'                => 'género',
+        'year'                  => 'año',
+        'month'                 => 'mes',
+        'day'                   => 'día',
+        'hour'                  => 'hora',
+        'minute'                => 'minuto',
+        'second'                => 'segundo',
+        'title'                 => 'título',
+        'body'                  => 'contenido',
+        'description'           => 'descripción',
+        'excerpt'               => 'extracto',
+        'date'                  => 'fecha',
+        'time'                  => 'hora',
+        'subject'               => 'asunto',
+        'message'               => 'mensaje',
+        'credit_card'           => 'tarjeta de crédito',
+        'debit_card'            => 'tarjeta de débito',
+        'url'                   => 'dirección web',
+        'date_range'            => 'rango de fechas',
+        'tags'                  => 'etiquetas',
+        'date_time'             => 'fecha y hora',
+    ],
+
+];
